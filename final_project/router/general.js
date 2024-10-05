2a799c72db4c468d92071d83391bb076b0114224
Alto0327 <aj.silvaf2003@gmail.com>
2024-10-05 10:49:00 -0400

    Final Project completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,19 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password){
+    if(!isValid(username)){
+      users.push({"username": username, "password": password});
+      return res.status(200).json({message: "User registered successfully"});
+    }else{
+      return res.status(404).json({message: "user already exists!"})
+    }
+  }
+  return res.status(404).json({message: "username and password are required"});
+ 
 });
 
 // Get the book list available in the shop
@@ -53,7 +65,14 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let review = books[isbn].reviews;
+
+  if(review){
+    res.send(review);
+  }else{
+    return res.status(404).json({message:"Book not found"})
+  }
 });
 
 module.exports.general = public_users;
