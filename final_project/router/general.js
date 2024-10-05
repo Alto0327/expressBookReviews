616af13bd1e1de603322e80ca4cd4bbe6f715f21
Alto0327 <aj.silvaf2003@gmail.com>
2024-10-05 09:48:20 -0400

    Details with ISBN, author and title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,19 +19,35 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let filteredAuthor = Object.values(books).filter((book) => book.author === author);
+  
+  if(filteredAuthor.length >0){
+    res.send(filteredAuthor);
+  }else{
+    return res.status(404).json({message: "Book not found"});
+  }
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  let filteredTitle = Object.values(books).filter((book) => book.title === title);
+  
+  if(filteredTitle.length >0){
+    res.send(filteredTitle);
+  }else{
+    return res.status(404).json({message: "Book not found"});
+  }
 });
 
 //  Get book review
